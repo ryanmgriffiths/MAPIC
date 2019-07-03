495c2f7e509c4afa9e54d2ffdde9f0f9750fb58d
Ryan Michael Griffiths <ryan.michael.griffiths@cern.ch>
2019-07-03 13:07:48 +0000

    Update serialint.py, main.py files

--- main.py
+++ main.py
@@ -4,60 +4,41 @@ import pyb
 import time
 from array import array
 
-#Filtering - likely unnecessary
-"""
-def fltlow(element):
-    return element > 1000
-"""
-
-#constants
+#measurement time constants
 m_time = 0.001
 freqmax = 1000000
+#Diagnositcs constants
 a = 0
 b = 0
 
 #Define objects
 adc = pyb.ADC(pyb.Pin.board.X12)
-#rtc = RTC()
 t = pyb.Timer(1,freq=freqmax)
 usb = pyb.USB_VCP()
-time.sleep(10)
-well = usb.isconnected()
 
-
-#Interrupt code
-
-"""
-l = [0]*10
-buf = array("H",l)
-
-
-def callback(line):
-    adc.read_timed(buf,t)
-    print(buf)
-
-extint = pyb.ExtInt(pyb.Pin.board.X12, pyb.ExtInt.IRQ_RISING, pyb.Pin.PULL_NONE, callback)
-"""
-
+#Wait for serial connection to open
+while True:
+    if usb.isconnected():
+        break
+    else:
+        time.sleep(0.5)
 
 #Setup variables/file/buffer
-
 l = [0]*int(freqmax*m_time)
 buf = array("H",l)
-
 list_time = []
 
+#Take readings
 for rd in range(1):
-
-    a = utime.ticks_us()
+    #a = utime.ticks_us()
     adc.read_timed(buf,t)
     usb.write(buf)
-    b = utime.ticks_us() - a
+    #b = utime.ticks_us() - a
     list_time.append(b)
 
+usb.write('end')#end condition for pyserial read.
 
-print('')
-
+#end flash
 led1 = pyb.LED(2)
 led1.on()
 for x in range(15):
--- serialint.py
+++ serialint.py
@@ -1,12 +1,6 @@
 import serial
 import time
 
-
-ser = serial.Serial('COM5', 115200, timeout=1)
-ser.open()
-
-ser.send('ready')
-
-time.sleep(10)
-
-data = ser.readlines()
+ser = serial.Serial('COM5', 115200)
+raw_data = ser.read_until('end')
+print(raw_data)
