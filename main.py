f8f5a00c61deb399e114b768265f2d608eb22370
Ryan Michael Griffiths <ryan.michael.griffiths@cern.ch>
2019-07-05 14:54:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,54 +5,51 @@ import time
 from array import array
 import utime
 
-#measurement time constants
-m_time = 0.001
-freqmax = 1000000
-#Diagnositcs constants
-a = 0
-b = 0
-
-#Define objects
-adc = pyb.ADC(pyb.Pin.board.X12)
-t = pyb.Timer(1,freq=freqmax)
 usb = pyb.USB_VCP()
-led = pyb.LED(1)
 
-#Wait for serial connection to open
 while True:
-    if usb.isconnected():
-        break
+
+    if mode == 'ADC_read':
+        freqmax = 1000000
+        t = pyb.Timer(1,freq=freqmax)
+
+        #Successful connection
+        '''
+        readings = usb.recv(4,timeout=5000)
+        readings = array('L',readings)
+        '''
+
+        readings=1000
+        led.toggle()
+
+        #Setup variables/file/buffer
+        buf = array("H",[0]*1000)
+
+        #Take readings
+        for x in range(readings):
+            adc.read_timed(buf,t)
+            usb.write(buf)
+
+    elif mode == 'I2C_change':
+        if:
+        
+        elif:
+
+            i2c = pyb.I2C(1, I2C.MASTER,baudrate=400000)
+
+            b = bytearray([0x00,255])
+
+    elif mode == 'I2C_read':
+        
+        i2c = pyb.I2C(1, I2C.MASTER,baudrate=400000)
+
+
+
+
     else:
-        time.sleep(0.1)
+        pass
 
-#Successful connection
-led.toggle()
-time.sleep(1)
 
-'''
-readings = usb.recv(4,timeout=5000)
-readings = array('L',readings)
-'''
 
-readings=1000
-led.toggle()
 
-#Setup variables/file/buffer
-l = [0]*int(freqmax*m_time)
-buf = array("H",l)
-s1 = 'end'
-endbuff = bytes('end','utf-8')
-
-#list_time = []
-
-#Take readings
-for x in range(readings):
-    adc.read_timed(buf,t)
-    usb.write(buf)
-
-#end flash
-led1 = pyb.LED(2)
-led1.on()
-for x in range(15):
-    led1.toggle()
-    time.sleep(0.05)
+    
