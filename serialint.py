a9f981a30d91756fa10e4a3da2a0e6683f54c077
Ryan Michael Griffiths <ryan.michael.griffiths@cern.ch>
2019-07-05 12:46:52 +0000

    Update serialint.py

--- serialint.py
+++ serialint.py
@@ -1,16 +1,33 @@
 import serial
 import time
+from array import array
+import numpy
+import matplotlib.pyplot as plt
+import sys
 
-ser = serial.Serial('COM5', 115200,timeout=1)
+readings = 1000
+data = numpy.zeros((readings,1000),dtype='uint16')
+ser = serial.Serial('COM7', 115200,timeout=10)
+'''a = array('L',[readings])
+ser.write(a)
+print(sys.getsizeof(a))
+'''
+a = time.time()
 
-file = open('rawdata.txt','w')
-#for x in range(100):
-time.sleep(10)
+for x in range(readings):
+    read = ser.read(2000)
+    #data[x,:] = numpy.frombuffer(read,dtype='uint16')
 
-raw_data = ser.read_until(bytes('end','utf-8'))
+b = time.time()
+print('Done!',b-a)
 
-file.write(str(raw_data))
+datalong = data.flatten()
+x = numpy.arange(len(datalong))
 
-file.close()
+plt.figure()
+plt.plot(x,datalong)
+plt.show()
 
+
+numpy.savetxt('data.txt',data)
 
