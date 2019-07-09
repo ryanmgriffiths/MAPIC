08bfee5a9350b7e2aed0660673409a0287dbcbd9
Ryan Michael Griffiths <ryan.michael.griffiths@cern.ch>
2019-07-09 16:31:53 +0000

    APIC success with I2C

--- dAPIC.py
+++ dAPIC.py
@@ -3,45 +3,71 @@ import time
 from array import array
 import numpy
 import matplotlib.pyplot as plt
-import sys
-import warnings
 
-ser = serial.Serial('COM4', 115200,timeout=10)
-gain = '0x2C'
-width = 0x2C
 
-def ADC():
-    readings = input('Size >')
-    data = numpy.zeros((readings,1000),dtype='uint16')
+#import sys
+#import warnings
 
-    for x in range(readings):
-        read = ser.read(2000)
-        data[x,:] = numpy.frombuffer(read,dtype='uint16')
+ser = serial.Serial('COM7', 115200,timeout=10)
 
-    return data
 
 while True:
 
-    cmd = bytes(input('>'),'utf-8')
+    cmd = input('>')
 
-    if cmd == 'I2C_r':
-        
-        address = address = input('gain/width:\n>')
-        ser.write('I2C_r\n')
-        ser.write(address+'\n')
-        while ser.inwaiting() == 0:
-            pass
-        print(int.from_bytes(ser.readline(),'big'))
-    
-    elif cmd == 'I2C_w':
-        
+    if cmd == 'Ir':
+
+        sercom = bytearray([0,0])
         address = input('gain/width:\n>')
-        value = int(input('Value:\n>'))
-        ser.write('I2C_w\n')
-        ser.write(address+'\n')
-        ser.write(bytes(value))
+        
+        if address == 'gain':
+            address = 0
+        elif address == 'width':
+            address = 1
 
+        sercom[1] = address
+
+        ser.write(sercom)
+        reply = ser.read(1)
+        print(int.from_bytes(reply,'big'))
+
+    elif cmd == 'Iw':
+
+        sercom = bytearray([1,0])
+        address = input('gain/width:\n>')
+        value = int(input('value\n>'))
+        
+        if address == 'gain':
+            address = 0
+        elif address == 'width':
+            address = 1
+        
+        sercom[1] = address
+        ser.write(sercom)
+        ser.write(bytes([value]))
+    
+    elif cmd == 'Is':
+        sercom = bytearray([0,2])
+        ser.write(sercom)
+        print(ser.read(1))
+    
+    elif cmd == 'A':
+        sercom = bytearray([2,0])
+        reads = int(input('Readings:\n>'))
+        num_reads = (reads).to_bytes(4,byteorder='big')
+        ser.write(sercom)
+        ser.write(num_reads)
+        data = numpy.zeros((num_reads,1000),dtype='uint16')
+        
+        for x in range(num_reads):
+            read = ser.read(2000)
+            data[x,:] = numpy.frombuffer(read,dtype='uint16')
+        numpy.savetxt('data.txt',data)
     else:
-        print('Invalid command.') 
+        print('Invalid command.')
+    
     
     print('Complete')
+    
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
