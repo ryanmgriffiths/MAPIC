cb70bca2ca0436d55659d3e65bb63bb02c62dcd0
Ryan Michael Griffiths <ryan.michael.griffiths@cern.ch>
2019-07-16 12:35:56 +0000

    Added requirements file.

--- hist_generator.py
+++ hist_generator.py
@@ -2,36 +2,39 @@ from __future__ import division
 import numpy
 import matplotlib.pyplot as plt
 
-raw_data = numpy.loadtxt('datawifi.txt')
-rmsvals = numpy.array([])
-flatdata = numpy.flatten(raw_data)
+
+def poll_alg(fname):
+    raw_data = numpy.loadtxt(fname)
+    rmsvals = numpy.array([])
+    flatdata = numpy.flatten(raw_data)
 
 
-sliced = numpy.array([])
+    sliced = numpy.array([])
 
-for idx,x in enumerate(flatdata):
-    
-    if x > 200 and x not in sliced:
-        del sliced[:]
-        start = idx
-        val = x
-        numpy.append(sliced,val)
-        v = 1
+    for idx,x in enumerate(flatdata):
         
-        while val > 200:
-            numpy.append(sliced,flatdata[idx+v])
-            v = v + 1
+        if x > 200 and x not in sliced:
+            del sliced[:]
+            start = idx
+            val = x
+            numpy.append(sliced,val)
+            v = 1
+            
+            while val > 200:
+                numpy.append(sliced,flatdata[idx+v])
+                v = v + 1
+            
+            rmsval = numpy.sqrt(numpy.average(sliced**2))
+            numpy.append(rmsvals,rmsval)
         
-        rmsval = numpy.sqrt(numpy.average(sliced**2))
-        numpy.append(rmsvals,rmsval)
-    
-    elif x > 200 and x in sliced:
-        pass
-    
-    else:
-        pass
+        elif x > 200 and x in sliced:
+            pass
+        
+        else:
+            pass
 
-plt.figure()
-plt.hist(x,20)
-plt.show()
-plt.savefig('histogram.png')
+    plt.figure()
+    plt.hist(x,20)
+    plt.show()
+    plt.savefig('histogram.png')
+def irq
